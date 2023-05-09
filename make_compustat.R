ed791d1e02c313764fee1e400021dcbac0cb49d7
Ian Gow <iandgow@gmail.com>
2023-05-09 08:22:11 -0400

    Added initial chapters and code.

---
+++ make_compustat.R
@@ -0,0 +1,87 @@
+library(tidyverse)
+library(DBI)
+library(dbplyr)
+
+start_date <- ymd("1960-01-01")
+end_date <- ymd("2021-12-31")
+
+# WRDS ----
+wrds <- dbConnect(RPostgres::Postgres(), bigint = "integer")
+
+funda_db <- tbl(wrds, in_schema("comp", "funda"))
+compustat <- 
+  funda_db |>
+  filter(
+    indfmt == "INDL" &
+      datafmt == "STD" &
+      consol == "C" &
+      datadate >= start_date & datadate <= end_date
+  ) |>
+  select(
+    gvkey, # Firm identifier
+    datadate, # Date of the accounting data
+    seq, # Stockholders' equity
+    ceq, # Total common/ordinary equity
+    at, # Total assets
+    lt, # Total liabilities
+    txditc, # Deferred taxes and investment tax credit
+    txdb, # Deferred taxes
+    itcb, # Investment tax credit
+    pstkrv, # Preferred stock redemption value
+    pstkl, # Preferred stock liquidating value
+    pstk, # Preferred stock par value
+    capx, # Capital investment
+    oancf # Operating cash flow
+  ) |>
+  collect()
+
+compustat <- 
+  compustat |>
+  mutate(
+    be = coalesce(seq, ceq + pstk, at - lt) +
+      coalesce(txditc, txdb + itcb, 0) -
+      coalesce(pstkrv, pstkl, pstk, 0),
+    be = if_else(be <= 0, as.numeric(NA), be)
+  )
+
+compustat <- 
+  compustat |>
+  mutate(year = year(datadate)) |>
+  group_by(gvkey, year) |>
+  filter(datadate == max(datadate)) |>
+  ungroup()
+
+# SQLite ----
+tidy_finance <- dbConnect(
+  RSQLite::SQLite(),
+  "data/tidy_finance.sqlite",
+  extended_types = TRUE)
+
+dbWriteTable(tidy_finance,
+             "compustat",
+             value = compustat,
+             overwrite = TRUE
+)
+
+res <- dbSendQuery(tidy_finance, "VACUUM")
+res
+dbClearResult(res)
+dbDisconnect(tidy_finance)
+
+# DuckDB ----
+tidy_finance <- dbConnect(
+  duckdb::duckdb(),
+  "data/tidy_finance.duckdb",
+  read_only = FALSE
+)
+
+dbWriteTable(tidy_finance,
+             "compustat",
+             value = compustat,
+             overwrite = TRUE
+)
+
+dbDisconnect(tidy_finance, shutdown = TRUE)
+
+# RDS ----
+write_rds(compustat, "data/compustat.rds")
---
+++ make_crsp_daily.R
@@ -0,0 +1,73 @@
+library(tidyverse)
+library(DBI)
+library(dbplyr)
+
+start_date <- ymd("1960-01-01")
+end_date <- ymd("2021-12-31")
+
+# WRDS, SQLite ----
+wrds <- dbConnect(RPostgres::Postgres(), bigint = "integer")
+
+tidy_finance <- dbConnect(
+  RSQLite::SQLite(),
+  "data/tidy_finance.sqlite",
+  extended_types = TRUE)
+
+dsf_db <- tbl(wrds, in_schema("crsp", "dsf"))
+
+factors_ff_daily <- tbl(tidy_finance, "factors_ff_daily") |>
+  collect()
+
+permnos <- tbl(tidy_finance, "crsp_monthly") |>
+  distinct(permno) |>
+  pull()
+
+progress <- txtProgressBar(
+  min = 0,
+  max = length(permnos),
+  initial = 0,
+  style = 3
+)
+
+for (j in 1:length(permnos)) {
+  permno_sub <- permnos[j]
+  crsp_daily_sub <- dsf_db |>
+    filter(permno == permno_sub &
+             date >= start_date & date <= end_date) |>
+    select(permno, date, ret) |>
+    collect() |>
+    drop_na()
+  
+  if (nrow(crsp_daily_sub) > 0) {
+    crsp_daily_sub <- crsp_daily_sub |>
+      mutate(month = floor_date(date, "month")) |>
+      left_join(factors_ff_daily |>
+                  select(date, rf), by = "date") |>
+      mutate(
+        ret_excess = ret - rf,
+        ret_excess = pmax(ret_excess, -1)
+      ) |>
+      select(permno, date, month, ret_excess)
+    
+    dbWriteTable(tidy_finance,
+                 "crsp_daily",
+                 value = crsp_daily_sub,
+                 overwrite = ifelse(j == 1, TRUE, FALSE),
+                 append = ifelse(j != 1, TRUE, FALSE)
+    )
+  }
+  setTxtProgressBar(progress, j)
+}
+
+close(progress)
+
+# DuckDB ----
+crsp_daily_db <- tbl(tidy_finance, "crsp_daily")
+
+ddb <- dbConnect(
+  duckdb::duckdb(),
+  "data/tidy_finance.duckdb",
+  read_only = FALSE
+)
+
+dbDisconnect(ddb, shutdown = TRUE)
