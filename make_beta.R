2a4a40f0a5cf59630552f8d5e431ac77bdeedb4b
Ian Gow <iandgow@gmail.com>
2023-05-11 08:22:47 -0400

    Make beta code files.

---
+++ make_beta.R
@@ -0,0 +1,96 @@
+library(tidyverse)
+library(DBI)
+library(slider)
+library(furrr)
+
+tidy_finance <- dbConnect(
+  RSQLite::SQLite(),
+  "data/tidy_finance.sqlite",
+  extended_types = TRUE)
+
+crsp_monthly <- tbl(tidy_finance, "crsp_monthly") |>
+  collect()
+
+factors_ff_monthly <- tbl(tidy_finance, "factors_ff_monthly") |>
+  collect()
+
+crsp_monthly <- crsp_monthly |>
+  left_join(factors_ff_monthly, by = "month") |>
+  select(permno, month, industry, ret_excess, mkt_excess)
+
+crsp_monthly_nested <- crsp_monthly |>
+  nest(data = c(month, ret_excess, mkt_excess))
+
+plan(multisession, workers = availableCores())
+
+estimate_capm <- function(data, min_obs = 1) {
+  if (nrow(data) < min_obs) {
+    beta <- as.numeric(NA)
+  } else {
+    fit <- lm(ret_excess ~ mkt_excess, data = data)
+    beta <- as.numeric(coefficients(fit)[2])
+  }
+  return(beta)
+}
+
+roll_capm_estimation <- function(data, months, min_obs) {
+  data <- data |>
+    arrange(month)
+  
+  betas <- slide_period_vec(
+    .x = data,
+    .i = data$month,
+    .period = "month",
+    .f = ~ estimate_capm(., min_obs),
+    .before = months - 1,
+    .complete = FALSE
+  )
+  
+  return(tibble(
+    month = unique(data$month),
+    beta = betas
+  ))
+}
+
+beta_monthly <- crsp_monthly_nested |>
+  mutate(beta = future_map(
+    data, ~ roll_capm_estimation(., months = 60, min_obs = 48)
+  )) |>
+  unnest(c(beta)) |>
+  select(permno, month, beta_monthly = beta) |>
+  drop_na()
+
+crsp_daily <- tbl(tidy_finance, "crsp_daily") |>
+  collect()
+
+factors_ff_daily <- tbl(tidy_finance, "factors_ff_daily") |>
+  collect()
+
+crsp_daily <- 
+  crsp_daily |>
+  inner_join(factors_ff_daily, by = "date") |>
+  select(permno, month, ret_excess, mkt_excess)
+
+crsp_daily_nested <-
+  crsp_daily |>
+  nest(data = c(month, ret_excess, mkt_excess))
+
+beta_daily <- 
+  crsp_daily_nested |>
+  mutate(beta_daily = future_map(
+    data, ~ roll_capm_estimation(., months = 3, min_obs = 50)
+  )) |>
+  unnest(c(beta_daily)) |>
+  select(permno, month, beta_daily = beta) |>
+  drop_na()
+
+beta <- beta_monthly |>
+  full_join(beta_daily, by = c("permno", "month")) |>
+  arrange(permno, month)
+
+dbWriteTable(tidy_finance,
+             "beta",
+             value = beta,
+             overwrite = TRUE)
+
+dbDisconnect(tidy_finance)
---
+++ make_beta_alt.R
@@ -0,0 +1,65 @@
+library(tidyverse)
+library(DBI)
+library(dbplyr)
+
+ddb <- dbConnect(
+  RSQLite::SQLite(),
+  "data/tidy_finance.sqlite",
+  extended_types = TRUE)
+
+crsp_monthly <- tbl(ddb, "crsp_monthly") 
+crsp_daily <- tbl(ddb, "crsp_daily") 
+
+factors_ff_monthly <- tbl(ddb, "factors_ff_monthly")
+factors_ff_daily <- tbl(ddb, "factors_ff_daily")
+
+window <- "OVER (PARTITION BY permno ORDER BY month
+           RANGE BETWEEN INTERVAL 59 MONTHS PRECEDING
+              AND INTERVAL 0 MONTHS FOLLOWING)"
+
+beta_sql <- sql(paste("regr_slope(ret_excess, mkt_excess)", window))
+n_sql <- sql(paste("count(*)", window))
+max_sql <- sql(paste("max(month)", window))
+min_sql <- sql(paste("min(month)", window))
+
+beta_monthly <- 
+  crsp_monthly |>
+  left_join(factors_ff_monthly, by = "month") |>
+  mutate(beta_monthly = beta_sql,
+         n = n_sql,
+         max_month = max_sql,
+         min_month = min_sql) |>
+  ungroup() |>
+  # Check that months are consecutive
+  mutate(n_months = date_diff('month', min_month, max_month) + 1) |>
+  filter(n >= 48, n_months == n) |>
+  select(permno, month, beta_monthly) |>
+  filter(!is.na(beta_monthly)) %>%
+  compute()
+
+window <- "OVER (PARTITION BY permno ORDER BY month
+           RANGE BETWEEN INTERVAL 2 MONTHS PRECEDING
+              AND INTERVAL 0 MONTHS FOLLOWING)"
+
+beta_sql <- sql(paste("regr_slope(ret_excess, mkt_excess)", window))
+n_sql <- sql(paste("count(*)", window))
+
+beta_daily <- 
+  crsp_daily |>
+  left_join(factors_ff_daily, by = "date") |>
+  select(permno, month, ret_excess, mkt_excess) |>
+  filter(!is.na(ret_excess)) |>
+  mutate(beta_daily = beta_sql, n = n_sql) %>%
+  filter(n >= 50) %>%
+  select(permno, month, beta_daily) %>%
+  distinct()
+
+dbExecute(ddb, "DROP TABLE IF EXISTS beta_alt")
+
+beta_alt <- 
+  beta_monthly |>
+  full_join(beta_daily, by = c("permno", "month")) |>
+  arrange(permno, month) %>%
+  compute(name = "beta_alt", temporary = FALSE)
+
+dbDisconnect(ddb, shutdown = TRUE)
